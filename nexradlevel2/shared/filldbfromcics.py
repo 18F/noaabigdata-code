74e6fa7206f548132865baba3c3cfc6adf59729c
Alan Steremberg <alan.steremberg@gsa.gov>
2015-06-25 10:22:26 -0400

    changes

--- nexradlevel2/shared/filldbfromcics.py
+++ nexradlevel2/shared/filldbfromcics.py
@@ -2,15 +2,10 @@ import sqlite3
 conn = sqlite3.connect('nexradl2.db')
 c = conn.cursor()
 
-# Create table
-#c.execute('''CREATE TABLE files
-#                 (path text, azure text, aws text, google text, ibm text, occ text)''')
-
 
 #open the file list and look for files
 
 # Insert a row of data
-#c.execute("INSERT INTO stocks VALUES ('2006-01-05','BUY','RHAT',100,35.14)")
 filename = "i3.txt"
 with open(filename) as f:
   for line in f:
