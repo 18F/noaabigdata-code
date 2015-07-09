f1149a44cefc4252019b5eb2e95fc9ab346af71d
Alan Steremberg <alan.steremberg@gsa.gov>
2015-07-09 14:36:47 -0400

    upgrades

--- nexradlevel2/shared/filldbfromcics.py
+++ nexradlevel2/shared/filldbfromcics.py
@@ -1,13 +1,29 @@
 import sqlite3
-conn = sqlite3.connect('nexradl2aws.db')
+import sys
+
+db_name = 'nexradl2.db'
+
+#
+#  sys.argv[1]: dbname
+#  sys.argv[2]: dbname
+#  sys.argv[3]: nopath
+#
+
+if len(sys.argv) > 1:
+  print sys.argv
+  db_name = sys.argv[1]
+
+print db_name
+
+conn = sqlite3.connect(db_name)
 c = conn.cursor()
 
 
 #open the file list and look for files
 
 # Insert a row of data
-#filename = "i3.txt"
-filename = "2012-a.txt"
+filename = "i3.txt"
+#filename = "2012-a.txt"
 with open(filename) as f:
   for line in f:
      com = line.strip().split(',')
