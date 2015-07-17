bd7cf56f01995d4eeb8bad4705d4afb8ae48204e
Alan Steremberg <alan.steremberg@gsa.gov>
2015-07-17 18:27:44 -0400

    updating scripts

--- nexradlevel2/shared/cicsfiledump.sh
+++ nexradlevel2/shared/cicsfiledump.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 #find /snfs9/q2/levelii_tarfiles/ -printf "%h/%f,%s\n" | grep "\.tar" | grep -v "_2012" > i3.txt
-find /snfs9/q2/levelii_tarfiles/2010??  -printf "%h/%f,%s\n" | grep "\.tar"  > i3.txt
+#find /snfs9/q2/levelii_tarfiles/2010??  -printf "%h/%f,%s\n" | grep "\.tar"  > i3.txt
+find /snfs9/q2/levelii_tarfiles/2014??  -printf "%h/%f,%s\n" | grep "\.tar"  > i3.txt
--- nexradlevel2/shared/updatedbwithfill.sh
+++ nexradlevel2/shared/updatedbwithfill.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 # run this script to take i3.txt and update each database 
 # when it is done we can  launch the uploaders 
+./cicsfiledump.sh
+wc -l i3.txt
 nohup python filldbfromcics.py  ../aws/nexradl2aws.db &
 nohup python filldbfromcics.py ../ms/nexradl2.db &
 
