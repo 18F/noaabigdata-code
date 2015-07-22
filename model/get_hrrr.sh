d37f6347d3e1ed3dc0871c0d9abeebc053e82d2d
Alan Steremberg <alan.steremberg@gsa.gov>
2015-07-22 10:05:19 -0400

    changes

--- model/get_hrrr.sh
+++ model/get_hrrr.sh
@@ -11,7 +11,8 @@ mkdir -p ${outdir}
 baseurl="http://nomads.ncep.noaa.gov/pub/data/nccf/nonoperational/com/hrrr/prod/hrrr."${datestring}
 for HH in `seq -w 0 1 15` ; do
   #for output_type in "wrfnat" "wrfsubh" "wrfprs" "wrfsfc" ; do #get all HRRR types, very big
-  for output_type in "wrfsubh" "wrfsfc" ; do
+  #for output_type in "wrfsubh" "wrfsfc" ; do
+  for output_type in "wrfnat" "wrfsubh" "wrfprs" "wrfsfc" ; do
     srcurl=${baseurl}/hrrr.t${hourstring}z.${output_type}f${HH}.grib2
     echo ${srcurl}
     wget -qN -P ${outdir} ${srcurl} &
